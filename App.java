87efd26f335967cab665dcdebb3cd93c49d5cd6b
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-02-17 22:12:35 +0900

    created "title" on this game

--- App.java
+++ App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 600,
         int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
     }
 }
