50c6417abdda17353400e68b6b5cac0ece1092c9
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-02-17 22:26:32 +0900

    fix typo

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int boardWidth = 600,
+        int boardWidth = 600;
         int boardHeight = boardWidth;
 
         JFrame frame = new JFrame("Snake");
