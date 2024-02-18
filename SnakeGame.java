5c856100a2d11b60d7950bd5590df1e19c5fdfdd
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-02-18 13:46:12 +0900

    create private class Tile

--- SnakeGame.java
+++ SnakeGame.java
@@ -5,6 +5,10 @@ import java.util.Random;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+    }
     int boardWidth;
     int boardHeight;
     int tileSize = 25;
