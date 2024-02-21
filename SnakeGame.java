98634786d8bdca1a1ab19e0484f46960af310979
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-02-21 19:48:20 +0900

    add code,KeyListener in public class SnakeGame

--- SnakeGame.java
+++ SnakeGame.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame extends JPanel implements ActionListener {
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     private class Tile {
         int x;
         int y;
@@ -42,7 +42,7 @@ public class SnakeGame extends JPanel implements ActionListener {
         random = new Random();
         placeFood();
 
-        velocityX = -1;
+        velocityX = 0;
         velocityY = 0;
 
         gameLoop = new Timer(100, this);
